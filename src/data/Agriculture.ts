8c16734926b5c46b3e358e29cda3a2432c6315fb
Bauaser-kun <michal.bala.1980@gmail.com>
2026-07-22 22:30:36 +0200

    converted agriculture tasks to acceptable by applicaion

---
+++ src/data/Agriculture.ts
@@ -0,0 +1,58 @@
+import type { TaskDefinition } from "../shared/types/TaskDefinition";
+
+export const agricultureTasks: TaskDefinition[] = [
+    {
+        id: 1,
+        islandId: "agriculture",
+        title: "Plant a Tree",
+        description: "Plant a seed and keep observation notes. "
+    },
+    {
+        id: 2,
+        islandId: "agriculture",
+        title: "plant proffessor",
+        description: "Identify 5 plants growing in your local area. "    
+    },    
+    {
+        id: 3,
+        islandId: "agriculture",
+        title: "plantator guide",
+        description: "Prepare plant care instructions. "    
+    },    
+    {
+        id: 4,
+        islandId: "agriculture",
+        title: "passing seasons",
+        description: "Draw the same field during the four seasons of the year."    
+    },    
+    {
+        id: 5,
+        islandId: "agriculture",
+        title: "local shops map",
+        description: "For a strange reason, all vehicles are out of service. Everyone must now buy supplies from local producers. Create a map locating all producers you can reach on foot or by bicycle from school or from home."    
+    },    
+    {
+        id: 6,
+        islandId: "agriculture",
+        title: "seasonal fruits and veggies",
+        description: "Create a quiz to find out which fruits and vegetables should be eaten in each season."    
+    },    
+    {
+        id: 7,
+        islandId: "agriculture",
+        title: "farming history",
+        description: "Find images of farming past and present and make a classroom exhibit."    
+    },    
+    {
+        id: 8,
+        islandId: "agriculture",
+        title: "seasonal shopping",
+        description: "Shop only local seasonal foods for a week and try to avoid packaging."    
+    },    
+    {
+        id: 9,
+        islandId: "agriculture",
+        title: "gardening flyer",
+        description: "Share simple garden tips to grow plants without chemicals in a flyer."    
+    }
+];
