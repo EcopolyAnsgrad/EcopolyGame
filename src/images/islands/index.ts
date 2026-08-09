fac54650f9d8370704e473aedbfeb47f0618c9c7
Bauaser-kun <michal.bala.1980@gmail.com>
2026-08-09 23:08:41 +0200

    changed imports paths in indexes

--- src/images/islands/index.ts
+++ src/images/islands/index.ts
@@ -1,9 +1,9 @@
-import agriculture from "./IslandAgriculture.png";
+import agriculture from "./islandAgriculture.png";
 import climate from "./IslandClimate.png";
 import everyday from "./IslandEveryday.png";
-import jobs from "./IslandJobs.png";
+import jobs from "./islandJobs.png";
 import nature from "./IslandNature.png";
-import recycling from "./IslandRecycling.png";
+import recycling from "./islandRecycling.png";
 import resources from "./IslandResources.png";
 
 export {
--- src/images/logos/index.ts
+++ src/images/logos/index.ts
@@ -1,10 +1,10 @@
-import ansgard from './ANSGRAD_LOGO.png';
-import erasmus from './erasmusplus.png';
-import horizontal from './EU_LOGO_RGB_NEG.png';
-import vertical from './EU_LOGO_RGB_NEG_VERTICAL.png';
-import gat from './GAT.png';
-import krotoszyn from './Krotoszyn.png';
-import tiraqueau from './TIRAQUEAU.png';
+import ansgard from "./ANSGRAD_LOGO.png";
+import erasmus from "./erasmusplus.png";
+import horizontal from "./EU_LOGO_RGB_NEG.png";
+import vertical from "./EU_LOGO_RGB_NEG_VERTICAL.png";
+import gat from "./GAT.png";
+import krotoszyn from "./Krotoszyn.png";
+import tiraqueau from "./TIRAQUEAU.png";
 
 export {
     ansgard,
